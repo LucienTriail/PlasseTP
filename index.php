26fb27aafdb2a67d805434933abeedf6d4cd78d9
GMTE94 <GMTE94@S202-PC02>
2021-01-28 15:06:47 +0100

    ajout de la page d'accueil index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>1er pas</title>
+    </head>
+    <body>
+        <h1>Lien vers les sessions</h1>
+<a href="sessionsV.php"></a> 
+</body></html>
+
+   
--- vue/sessionsV.php
+++ vue/sessionsV.php
@@ -1,14 +1,35 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title> Premier pas </title>
+        <title>1er pas</title>
     </head>
     <body>
-        <h1> Fahem </h1>
 
-        <p> Les sessions sont : <?= count($session[]) ?> </p>
 
-    </body>
-</html>
+        <?php
+        if (count($sessions) != 0) {
+            ?>
+            <ol>
+                <p>Il y a  <?= count($sessions) ?> sessions:</p>
+                <?php
+                foreach ($sessions as $session) {
+                    ?>
+
+                    <li><a href="projets.php?idSession=<?= $session["idSession"] ?>"<a><?= $session["nom"] ?> 
+                            (n° <?= $session["idSession"] ?>)</li>
+                    <?php
+                }
+                ?>
+            </ol>
+            <?php
+        } else {
+            ?>
+
+            <p>Aucune session trouvée</p>
+            <?php
+        }
+        ?>
+    </body></html>
+
 
 
