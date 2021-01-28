641c5ff735bea8b9d9b2a235fcdcb4a4d3321bc2
GMTE94 <GMTE94@S202-PC02>
2021-01-28 14:33:21 +0100

    ajouts de projets et projetsV

---
+++ projets.php
@@ -0,0 +1,26 @@
+<?php
+
+//on recupere l'id_session
+$idSession = filter_input(INPUT_GET, "idSession", FILTER_VALIDATE_INT);
+//initialise les donnéees de la vue, des objets donc null
+$erreur = null;
+$session = null;
+//verifier que cest bon
+if ($idSession == null || $idSession == false) {
+    $erreur = "idSession invalide";
+} else {
+//appel du modèle
+    require_once 'modele/SessionDao.php';
+    $session = SessionDao:: getById($idSession);
+    if ($session == null) {
+        $erreur = "Session $idSession introuvable";
+    }
+}
+
+
+
+
+
+
+require_once 'Vue/indexV.php';
+?>
