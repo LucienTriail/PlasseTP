af719d57c2ded5843630aa99db735b25ce7ffe8a
GMTE94 <GMTE94@172.20.31.134>
2021-01-28 15:35:14 +0100

    Maj sessions

--- modele/SessionDao.php
+++ modele/SessionDao.php
@@ -1,33 +1,33 @@
 <?php
 
 class SessionDao {
-    
-    /** session de id $idSession (tableau association),
-     * ou null si pas trouvé
-     * @param type $idSession
-     */
-    public static function getById($idSession) {
-        $result = null;
-        if ($idSession == 1) {
-            // Un tableau associatif 
-            $result = array(
-                "id_session" => 1,
-                "nom" => "CDA 1"
-            );
-        }
-        return $result;
-    }
-    
-    public static function getAll() {
-        return array(
-            array(
-                "id_session_formation" => 1,
-                "nom" => "CDA 1"
-            ),
-                 array(
-                "id_session_formation" => 1,
-                "nom" => "CDA 1"
-            )); 
-    }
-}
 
+  /** session de id $idSession (tableau association),
+   * ou null si pas trouvé
+   * @param type $idSession
+   */
+  public static function getById($idSession) {
+    $result = null;
+    if ($idSession == 1) {
+      // Un tableau associatif
+      $result = array(
+          "id_session" => 1,
+          "nom" => "CDA 1"
+      );
+    }
+    return $result;
+  }
+
+  public static function getAll() {
+    return array(
+        array(
+            "id_session_formation" => 1,
+            "nom" => "CDA 1"
+        ),
+        array(
+            "id_session_formation" => 2,
+            "nom" => "CDI 7"
+    ));
+  }
+
+}
--- sessions.php
+++ sessions.php
@@ -1,14 +1,6 @@
 <?php
-
-
-// Appellons le modele
-require_once "modele/SessionDao";
-    $session = SessionDao::getAll();
-
-
-
-
-
-// inclure la vue indexV.php
+require_once "modele/sessionDao.php";
+$sessions = SessionDao::getAll();
 require_once "vue/sessionsV.php";
+
 ?>
